9b2bc1f98df3bde5c360df89bf9c699e4b27c3f6
LikeWho54 <95715728+LikeWho54@users.noreply.github.com>
2025-10-13 19:25:24 +0300

    analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 import HomePage from "./HomePage";
 import TermsOfService from "./Legal/TermsOfService";
 import PrivacyPolicy from "./Legal/PrivacyPolicy";
@@ -8,13 +9,16 @@ import DataProcessingAddendum from "./Legal/DataProcessingAddendum";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/legal/terms-of-service" element={<TermsOfService />} />
-      <Route path="/legal/privacy-policy" element={<PrivacyPolicy />} />
-      <Route path="/legal/cookie-policy" element={<CookiePolicy />} />
-      <Route path="/legal/data-processing-addendum" element={<DataProcessingAddendum />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/legal/terms-of-service" element={<TermsOfService />} />
+        <Route path="/legal/privacy-policy" element={<PrivacyPolicy />} />
+        <Route path="/legal/cookie-policy" element={<CookiePolicy />} />
+        <Route path="/legal/data-processing-addendum" element={<DataProcessingAddendum />} />
+      </Routes>
+      <Analytics />
+    </>
   );
 }
 
